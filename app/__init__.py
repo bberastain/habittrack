3401f2e1286c6512dcce3e9947045b5239492ed3
Bruno Berastain <bberastain@gmail.com>
2018-07-20 22:35:14 -0700

    bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -7,14 +7,14 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from config import Config
-# from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 login.login_view = 'auth.login'
 mail = Mail()
-# bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 
 
 def create_app(config_class=Config):
@@ -25,7 +25,7 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
-    # bootstrap.init_app(app)
+    bootstrap.init_app(app)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
