261bc91001628870cf3c02ae5b4b6deeef99b1b7
Bruno Berastain <bberastain@gmail.com>
2018-07-26 17:51:35 -0700

    accidentally deleted book log model

--- app/models.py
+++ app/models.py
@@ -68,8 +68,20 @@ class Habit(db.Model):
 
 class Completed(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date)  # the date of the view function?
+    date = db.Column(db.Date)
     habit_id = db.Column(db.Integer, db.ForeignKey('habit.id'))
 
     def __repr__(self):
         return '<Completed: id-{}, date-{}>'.format(self.id, self.date)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    author = db.Column(db.String(140))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Id: {}, Title: {}, Author: {}, Date Finished: {}>'.format(
+            self.id, self.title, self.author, self.date)
